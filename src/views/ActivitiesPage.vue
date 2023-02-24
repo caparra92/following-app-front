f8af221191f1eeb4f6390e0a0538a601a3a3e0f4
Camilo Parra <caparra92@gmail.com>
2023-02-24 16:49:46 -0500

    Activities page added

---
+++ src/views/ActivitiesPage.vue
@@ -0,0 +1,103 @@
+<template>
+ <ion-page>
+    <ion-content>
+        <ion-grid class="container">
+            <ion-row>
+                <ion-col>
+                    <ion-avatar>
+                        <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+                    </ion-avatar>
+                </ion-col>
+            </ion-row>
+            <ion-row>
+                <ion-col>
+                    <p>Camilo</p>
+                    <p>Parra</p>
+                </ion-col>
+            </ion-row>
+            <ion-row>
+                <ion-col>
+                    <div v-for="{ id, name } in categories" :key="id">
+                        <category-badge :activityType="name" view="activities"/>
+                    </div>
+                </ion-col>
+            </ion-row>
+            <ion-row class="power-off">
+                <ion-col>
+                    <button @click="logout">
+                        <ion-icon size = "large" :icon="powerOutline"></ion-icon>
+                    </button>
+                </ion-col>
+            </ion-row>
+        </ion-grid>
+    </ion-content>
+ </ion-page>
+</template>
+
+<script setup>
+import router from "../router";
+import { IonAvatar, IonPage, IonContent, IonIcon, IonGrid, IonRow, IonCol } from '@ionic/vue';
+import { powerOutline } from 'ionicons/icons'
+import CategoryBadge from '../components/CategoryBadge.vue';
+import { useStore } from '../stores/store';
+import { onMounted, ref } from 'vue';
+
+const categories = ref([])
+
+const store = useStore();
+
+onMounted(async()=> {
+ categories.value = await store.getActivities();
+});
+
+const logout = async() => {
+    const data = await store.logout();
+    router.push('Login');
+    console.log(data);
+}
+
+</script>
+
+<style scoped>
+
+.container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 100%;
+    height: 100vh;
+    text-align: center;
+  }
+
+ion-avatar {
+    align-self: center;
+    margin: 0 auto;
+}
+
+ion-col p {
+    font-size: 30px;
+    margin: 0;
+}
+
+ion-col p:first-child {
+    color: var(--ion-color-primary);
+    font-weight: 600;
+    margin-top: 30px;
+}
+
+ion-col p:last-child {
+    color: var(--ion-color-medium);
+    font-weight: 400;
+    line-height: 1;
+    margin-bottom: 30px;
+}
+
+ion-col button {
+    background: none;
+}
+
+ion-icon {
+    margin-top: 30px;
+}
+
+</style>
