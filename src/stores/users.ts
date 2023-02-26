e97a975bc9ae7382cf37c4b01d4cdacb6675ef18
Camilo Parra <caparra92@gmail.com>
2023-02-25 21:56:28 -0500

    Users store created

---
+++ src/stores/users.ts
@@ -0,0 +1,46 @@
+import { defineStore } from 'pinia';
+import axios from 'axios';
+
+const token = localStorage.getItem('access_token') || null;
+
+const apiReq = axios.create({
+    baseURL: "http://localhost:3000/api",
+    headers: {
+      'Authorization': token
+    }
+  });
+
+export const useUsers = defineStore('users', {
+    state: () => {
+      return {
+        apiReq
+      }
+    },
+    getters: {
+      loggedIn() {
+        return token !== null
+      }
+    },
+    actions: {
+      async getUsers() {
+        try {
+          const { data } = await this.apiReq.get('/users');
+          return data;
+        } catch (error) {
+          throw `The api call failed with ${error}`;
+        }
+      },
+      async createUser(name: string, email: string, password: string) {
+        try {
+            const { data } = await this.apiReq.post('/users', {
+                name,
+                email,
+                password
+            });
+            return data;
+        } catch (error) {
+            return error;
+        }
+      }
+    },
+  })
