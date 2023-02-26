e3f36fe0f395cc4f3024b3453e34d55f9bb728c1
Camilo Parra <caparra92@gmail.com>
2023-02-26 17:49:51 -0500

    Base input component tested

---
+++ tests/unit/components/BaseInput.spec.ts
@@ -0,0 +1,30 @@
+import { mount, VueWrapper } from "@vue/test-utils";
+import BaseInput from "@/components/BaseInput.vue";
+
+
+describe('Base Input component', () => {
+    let wrapper: VueWrapper;
+    beforeEach(() => {
+        wrapper = mount(BaseInput, {
+            props: {
+                label: 'Email',
+                modelValue: 'caparra@mail.com'
+            }   
+        });
+    });
+
+    test('Should match with snapshot', () => {
+        expect(wrapper.html()).toMatchSnapshot();
+    });
+
+    test('Should define props in the component', () => {
+        expect(wrapper.props().label).toBe('Email');
+        expect(wrapper.props().modelValue).toBe('caparra@mail.com');
+    });
+
+    test('Should verify emmited event', () => {
+        const input = wrapper.find('input');
+        input.trigger('input');
+        expect(wrapper.emitted('update:modelValue')?.length).toBe(1);
+    });
+});
