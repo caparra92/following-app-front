5ab33ea28f5d5b0fdd5ddfa9cabe4da33121c775
Camilo Parra <caparra92@gmail.com>
2023-04-21 13:32:22 -0500

    Stats page modified to import logic from composable

--- src/views/StatsPage.vue
+++ src/views/StatsPage.vue
@@ -19,127 +19,14 @@
     </ion-page>
 </template>
 <script setup lang="ts">
-import 'swiper/css';
-import { onMounted, ref  } from 'vue';
-import { useRoute } from 'vue-router';
 import { IonPage, IonContent, IonGrid, IonRow, IonCol } from '@ionic/vue';
 import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Scrollbar } from 'swiper';
 import { BarChart } from 'vue-chart-3';
-import { Chart, registerables } from "chart.js";
 import MenuBadge from "@/components/MenuBadge.vue";
-import { months, days } from '../helpers/daysAndMonths';
-import { NewSet } from '../helpers/dataStructures';
-import { useHistories } from '@/stores/histories';
-import { useItems } from '@/stores/items';
+import getHistories from '@/composables/getHistories';
 
-interface UniqueMonthId {
-    month: string,
-    idx: number
-}
-
-const route = useRoute();
-const id = <string>route.params.id;
-const histories = useHistories();
-const items = useItems();
-let loaded = false;
-const activeIndex = ref(0);
-const modules = [Scrollbar];
-const categories = ref(<any[]>[]);
-const availableMonths = ref(<any[]>[]);
-const itemObj = ref(<any>{});
-const currentMonth = new Date().getUTCMonth() + 1;
-const uniqueMonths = new NewSet();
-const uniqueIds = new NewSet();
-const uniqueMonthId =  <UniqueMonthId[]>[];
-
-onMounted(async()=> {
-    const { item } = await items.getItem(id);
-    itemObj.value = item;
-    await histories.getHistoriesByMonth(id, currentMonth);
-    await histories.getHistoriesByItemId(id);
-    categories.value = histories.getHistories;
-    availableMonths.value = histories.getHistoriesById;
-    loaded = true;
-    getMonths(availableMonths.value).month.forEach((month) => uniqueMonths.add(month));
-    getMonths(availableMonths.value).id.forEach((id) => uniqueIds.add(id));
-    const sortedIds = <number[]>uniqueIds.values().sort((a: any,b: any)=> b - a );
-    for(let i = 0; i < sortedIds.length; i++) {
-        uniqueMonthId.push({
-            month: <string>uniqueMonths.values()[i],
-            idx: sortedIds[i]
-        });
-    }
-    activeIndex.value = uniqueMonthId.findIndex((month) => month.idx === currentMonth);
-    activeIndex.value === -1 ? selectItem(0,uniqueMonthId[0].idx) : selectItem(activeIndex.value,currentMonth);
-    console.log(activeIndex.value, currentMonth)
-    chartData.value.labels = getDayValue(categories.value).dayValue;
-    chartData.value.datasets[0].data = getDayValue(categories.value).value;
-  });
-
-const getMonths = (objData: any[]) => {
-    return {
-        month: objData.map(data => months[new Date(data.date).getUTCMonth()].name.substring(0,3)),
-        id: objData.map(data => months[new Date(data.date).getUTCMonth()].id)
-    }
-}
-
-const getDayValue = (objData: any[]) => {
-    const sortDays = objData.sort((a,b) => new Date(a.date).valueOf() - new Date(b.date).valueOf());
-    return {
-        dayWeek: sortDays.map(data => days[new Date(data.date).getUTCDay()].substring(0,3)),
-        dayValue: sortDays.sort((a,b) => a - b).map(data => [new Date(data.date).getUTCDate(), days[new Date(data.date).getUTCDay()].substring(0,3)]),
-        value: sortDays.map(data => data.value)
-    };
-}
-
-const selectItem = async(item: any, monthNumber: any) => {
-    if(activeIndex.value !== item) {
-        activeIndex.value = item;
-        await histories.getHistoriesByMonth(id, monthNumber);
-        categories.value = histories.getHistories;
-        chartData.value.labels = getDayValue(categories.value).dayValue;
-        chartData.value.datasets[0].data = getDayValue(categories.value).value;
-    } else {
-        return;
-    }
-}
-
-Chart.register(...registerables);
-
-const chartData: any = ref({
-      labels: [],
-      datasets: [
-        {
-          data: [],
-          backgroundColor: ['#FCDB00'],
-          borderRadius: 50,
-          borderSkipped: false
-        },
-      ],
-    });
-
-const options = ref({
-    responsive: true,
-    plugins: {
-        legend: {
-            display: false
-        }
-    },
-    scales: {
-        x: {
-            grid: {
-                display: false
-            },
-            border: {
-                display: false
-            }
-        },
-        y: {
-            display: false
-        }
-    }
-});
+const { modules, itemObj, uniqueMonthId, selectItem, activeIndex, loaded, chartData, options } = getHistories();
+// console.log(loaded.value)
 
 </script>
 <style scoped>
